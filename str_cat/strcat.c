f0a850e9337c64def4f18fba9185c6384c223f0d
wallaya <wallaya2zz8@gmail.com>
2026-08-19 14:54:05 +0100

    Add files via upload

---
+++ str_cat/strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char    *strcat(char *dest, char *src)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while(dest[i])
+        i++;
+    j = 0; 
+    while(src[j])
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int main()
+{
+    char    dest[20] = "How is everything ";
+    char    src[20] = "with ya?";
+
+    strcat(dest, src);
+    printf("%s\n", dest);
+}
